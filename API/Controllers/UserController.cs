9548d2a379010aa7b294dfb3d1f2a21927e68466
Juan Diego <juandsilva028@gmail.com>
2025-04-07 00:03:58 -0500

    Rest of the CRUD operations

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -10,21 +10,58 @@ namespace API.Controllers
     {
         private readonly AppDbContext context = context;
 
-    [HttpGet]	
-    //Good practice to use async/await for database queries
-    public async Task<ActionResult<List<User>>> GetUsers()
-    {
-        return await context.Users.ToListAsync();
-    }
+        [HttpGet]	
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return await context.Users.ToListAsync();
+        }
 
-    [HttpGet("{id}")]
-    public async Task<ActionResult<User>> GetUserDetail(string id)
-    {
-        var activity = await context.Users.FindAsync(id);
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserDetail(string id)
+        {
+            var activity = await context.Users.FindAsync(id);
 
-        if (activity == null) return NotFound();
-        
-        return activity;
-    }
+            if (activity == null) return NotFound();
+            
+            return activity;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(User user)
+        {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user.Id);
+        }
+
+        [HttpPut ("{id}")]
+        public async Task<ActionResult> UpdateUser(string id, User user)
+        {
+            var existingUser = await context.Users.FindAsync(id);
+
+            if (existingUser == null) return NotFound();
+
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            existingUser.Password = user.Password;
+            existingUser.Role = user.Role;
+
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteUser(string id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
