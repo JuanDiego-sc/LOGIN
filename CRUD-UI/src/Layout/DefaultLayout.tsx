05f8bb4f61f0039b036fe393ff922f039e3c8e6b
Juan Diego <juandsilva028@gmail.com>
2025-04-13 22:54:20 -0500

    delete unused import

--- CRUD-UI/src/Layout/DefaultLayout.tsx
+++ CRUD-UI/src/Layout/DefaultLayout.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../auth/authProvider";
-import { Modal } from "reactstrap";
 
 interface DefaultLayoutProps {
     children: React.ReactNode;
